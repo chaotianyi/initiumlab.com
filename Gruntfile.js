72eadb77193659bac6f3916913f811214ba76412
Andy Shu Xin <andyshuxin@gmail.com>
2015-09-03 17:50:50 +0800

    Change deployment with Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,75 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    clean: ['public'],
+
+    watch: {
+      app: {
+        files: ['source/**/*'],
+        options: {
+          livereload: true
+        },
+        tasks: ["build"]
+      }
+    },
+
+    connect: {
+      dist: {
+        options: {
+          port: 9000,
+          hostname: '0.0.0.0',
+          base: 'public/'
+        }
+      }
+    },
+
+    copy: {
+      dist: {expand: true,
+            flatten: false,
+            cwd: 'static',
+            src: ['**'],
+            dest: 'public/blog/',
+            filter: 'isFile'}
+    },
+
+    'gh-pages': {
+      options: {
+        base: 'public',
+        branch: 'gh-pages',
+        repo: 'https://github.com/initiumlab/initiumlab.com.git'
+      },
+      src: '**/*'
+    },
+
+    'exec': {
+      hexo_clean: 'hexo clean',
+      hexo_generate: 'hexo generate'
+    }
+
+
+  });
+
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  //grunt.loadNpmTasks('grunt-babel');
+  grunt.loadNpmTasks('grunt-exec');
+  grunt.loadNpmTasks('grunt-gh-pages');
+  grunt.loadNpmTasks('grunt-rsync');
+  grunt.loadNpmTasks('grunt-targethtml');
+
+  grunt.registerTask('build',  ['clean', 'exec:hexo_clean', 'exec:hexo_generate','copy']);
+  grunt.registerTask('serve',  ['build', 'connect', 'watch']);
+  grunt.registerTask('deploy', ['build', 'gh-pages']);
+};
